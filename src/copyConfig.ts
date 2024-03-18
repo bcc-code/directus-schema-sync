06dad7032957e30fbbb3ca8e73a846e540ab8527
HDDTHR <stevenjildeh@gmail.com>
2024-03-18 17:29:59 +0300

    Refactor copyConfig to allow for exiting the function without terminating the whole process

--- src/copyConfig.ts
+++ src/copyConfig.ts
@@ -2,20 +2,24 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-export async function copyConfig(force: boolean, { logger }: { logger: any }) {
+export async function copyConfig(force: boolean, exitOnFail: boolean, { logger }: { logger: any }) {
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const srcDir = path.resolve(__dirname, '../install');
   const targetDir = process.cwd();
+  let exit = false
 
   // Test if it doesn't already exist then if it does exit
   if (!force) {
     await fs.access(path.resolve(targetDir, 'schema-sync')).then(() => {
       logger.info('Config folder already exists, use --force to override');
-      process.exit(0);
+      if(exitOnFail)
+        process.exit(0);
+      exit = true;
     }).catch(() => {
       logger.info('Creating config folder...');
     });
   }
 
-  await fs.cp(srcDir, targetDir, { recursive: true });
+  if (!exit)
+    await fs.cp(srcDir, targetDir, { recursive: true });
 }
